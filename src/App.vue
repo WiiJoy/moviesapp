72245e68293e6db3624c4c456a1fa69b9777a49e
Vitaliy (work) <wiijoy@bk.ru>
2021-08-30 10:35:28 +0300

    Add poster background

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,33 @@
 <template>
   <div id="app">
-    <MoviesList :list="moviesList" />
+    <PosterBG :poster="posterBg"/>
+    <MoviesList :list="moviesList" @changePoster="onChangePoster"/>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from "vuex";
 import MoviesList from "@/components/MoviesList";
+import PosterBG from "@/components/PosterBG";
 
 export default {
   name: "App",
   components: {
     MoviesList,
+    PosterBG,
   },
+  data: () => ({
+    posterBg: "",
+  }),
   computed: {
     ...mapGetters("movies", ["moviesList"]),
   },
   methods: {
     ...mapActions("movies", ["fetchMovies"]),
+    onChangePoster(poster) {
+      console.log(poster);
+      this.posterBg = poster;
+    },
   },
 };
 </script>
@@ -27,5 +37,6 @@ export default {
   font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  position: relative;
 }
 </style>
--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -5,7 +5,7 @@
            <BRow>
             <template v-if="isExist">
                 <BCol cols="3" v-for="(movie, key) in list" :key="key">
-                    <MovieItem :movie="movie" />
+                    <MovieItem :movie="movie" @mouseover.native="onMouseOver(movie.Poster)"/>
                 </BCol>
             </template>
             <template v-else>
@@ -35,6 +35,11 @@ export default {
             return Boolean(Object.keys(this.list).length);
         },
     },
+    methods: {
+        onMouseOver(poster) {
+            this.$emit("changePoster", poster);
+        },
+    },
 }
 </script>
 
@@ -42,5 +47,6 @@ export default {
 .list-title {
     font-size: 50px;
     margin-bottom: 30px;
+    color: #fff;
 }
 </style>
